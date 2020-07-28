632ba8e146600e9afe57e19ac4fbe26eda444539
rod martin <rod@araknia.com>
2020-07-28 22:21:46 +0100

    Working for 2019.5 todo update the ECS systems to use current SystemBase version https://docs.unity3d.com/Packages/com.unity.entities@0.8/manual/ecs_systems.html?_ga=2.129264908.1422804139.1595954381-720770648.1549559399

---
+++ Assets/Scripts/ECS/Systems/TimedDestroySystem.cs
@@ -0,0 +1,85 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+using Unity.Mathematics;
+
+
+[UpdateAfter(typeof(MoveForwardSystem))]
+public class TimedDestroySystem : JobComponentSystem
+{
+	EndSimulationEntityCommandBufferSystem buffer;
+	
+	 // protected override void OnCreateManager()
+	 // {
+	 // 	buffer = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+	 // }
+
+	 protected override void OnCreate()
+	 {
+		 buffer = World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+
+	 }
+
+	 struct CullingJob : IJobForEachWithEntity<TimeToLive>
+	{
+		public EntityCommandBuffer.Concurrent commands;
+		public float dt;
+
+		public void Execute(Entity entity, int jobIndex, ref TimeToLive timeToLive)
+		{
+			timeToLive.Value -= dt;
+			if (timeToLive.Value <= 0f)
+				commands.DestroyEntity(jobIndex, entity);
+		}
+	}
+
+	protected override JobHandle OnUpdate(JobHandle inputDeps)
+	{
+		var job = new CullingJob
+		{
+			commands = buffer.CreateCommandBuffer().ToConcurrent(),
+			dt = Time.DeltaTime
+		};
+
+		var handle = job.Schedule(this, inputDeps);
+		buffer.AddJobHandleForProducer(handle);
+
+		return handle;
+	}
+}
+
+/*
+public class TimedDestroySystem : SystemBase
+{
+	public struct Position : IComponentData
+	{
+		public float3 Value;
+	}
+
+	public struct Velocity : IComponentData
+	{
+		public float3 Value;
+	}
+
+	protected override void OnUpdate()
+	{
+// Local variable captured in ForEach
+		float dT = Time.DeltaTime;
+
+		Entities
+			.WithName("Culling_Job")
+			.ForEach(
+				(ref Position position, in Velocity velocity) =>
+				{
+					position = new Position()
+					{
+						Value = position.Value + velocity.Value * dT
+					};
+				}
+			)
+			.ScheduleParallel();
+	}
+}
+*/
+
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,68 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+	[Header("Enemy Spawn Info")]
+	public bool spawnEnemies = true;
+	public bool useECS = false;
+	public float enemySpawnRadius = 10f;
+	public GameObject enemyPrefab;
+
+	[Header("Enemy Spawn Timing")]
+	[Range(1, 100)] public int spawnsPerInterval = 1;
+	[Range(.1f, 2f)] public float spawnInterval = 1f;
+	
+	EntityManager manager;
+	Entity enemyEntityPrefab;
+
+	float cooldown;
+// 2019.5 update
+	private Entity entityPrefab;
+	private World defaultWorld;
+	void Start()
+	{
+		if (useECS)
+		{
+			defaultWorld = World.DefaultGameObjectInjectionWorld;
+			manager = defaultWorld.EntityManager;
+		//	enemyEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(enemyPrefab, World.Active);
+		// 2019.5 update
+		GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(defaultWorld, null);
+		enemyEntityPrefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(enemyPrefab, settings);
+		}
+	}
+
+	void Update()
+    {
+		if (!spawnEnemies || Settings.IsPlayerDead())
+			return;
+
+		cooldown -= Time.deltaTime;
+
+		if (cooldown <= 0f)
+		{
+			cooldown += spawnInterval;
+			Spawn();
+		}
+    }
+
+	void Spawn()
+	{
+		for (int i = 0; i < spawnsPerInterval; i++)
+		{
+			Vector3 pos = Settings.GetPositionAroundPlayer(enemySpawnRadius);
+
+			if (!useECS)
+			{
+				Instantiate(enemyPrefab, pos, Quaternion.identity);
+			}
+			else
+			{
+				Entity enemy = manager.Instantiate(enemyEntityPrefab);
+				manager.SetComponentData(enemy, new Translation { Value = pos });
+			}
+		}
+	}
+}
